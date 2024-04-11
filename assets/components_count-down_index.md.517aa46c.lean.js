b6d27db8f386e69ae03723bfc1911fecb69ed52f
lcyh <lcyh@users.noreply.github.com>
2024-04-11 02:46:30 +0000

    deploy: f4337458993d977547c499e251e7aca7e25936cf

---
+++ assets/components_count-down_index.md.517aa46c.lean.js
@@ -0,0 +1,50 @@
+import{a4 as c,_ as m,E as D,c as h,H as y,C as i,A as t,a as o,S as E,o as _}from"./chunks/framework.32362502.js";const{defineComponent:B}=c,g=B({name:"component-doc",components:{"render-demo-0":function(){const{toDisplayString:n,createElementVNode:s,resolveComponent:u,createVNode:l,Fragment:r,openBlock:p,createElementBlock:e}=c;function a(F,V){const d=u("g-count-down");return p(),e(r,null,[s("p",null,n(F.msg),1),s("div",null,[l(d,{type:"button"})]),s("p",null,n(F.msg1),1),s("div",null,[l(d,{type:"input"})])],64)}const{defineComponent:C}=c,A=C({setup(){return{msg:" 倒计时按钮组件",msg1:" 倒计时按钮输入组件"}}});return{render:a,...A}}()}}),S=JSON.parse('{"title":"CountDown","description":"","frontmatter":{},"headers":[],"relativePath":"components/count-down/index.md","filePath":"components/count-down/index.md","lastUpdated":1712803500000}'),f=t("h1",{id:"countdown",tabindex:"-1"},[o("CountDown "),t("a",{class:"header-anchor",href:"#countdown","aria-label":'Permalink to "CountDown"'},"​")],-1),b=t("p",null,"倒计时组件",-1),v=t("h2",{id:"基本用法",tabindex:"-1"},[o("基本用法 "),t("a",{class:"header-anchor",href:"#基本用法","aria-label":'Permalink to "基本用法"'},"​")],-1),w=t("p",null,"组件文档示例",-1),x=t("div",{class:"language-vue"},[t("pre",{"v-pre":"",class:"shiki material-theme-palenight"},[t("code",null,[t("span",{class:"line"},[t("span",{style:{color:"#89DDFF"}},"<"),t("span",{style:{color:"#F07178"}},"template"),t("span",{style:{color:"#89DDFF"}},">")]),o(`
+`),t("span",{class:"line"},[t("span",{style:{color:"#A6ACCD"}},"  "),t("span",{style:{color:"#89DDFF"}},"<"),t("span",{style:{color:"#F07178"}},"p"),t("span",{style:{color:"#89DDFF"}},">"),t("span",{style:{color:"#A6ACCD"}},"{{ msg }}"),t("span",{style:{color:"#89DDFF"}},"</"),t("span",{style:{color:"#F07178"}},"p"),t("span",{style:{color:"#89DDFF"}},">")]),o(`
+`),t("span",{class:"line"},[t("span",{style:{color:"#A6ACCD"}},"  "),t("span",{style:{color:"#89DDFF"}},"<"),t("span",{style:{color:"#F07178"}},"div"),t("span",{style:{color:"#89DDFF"}},">")]),o(`
+`),t("span",{class:"line"},[t("span",{style:{color:"#A6ACCD"}},"    "),t("span",{style:{color:"#89DDFF"}},"<"),t("span",{style:{color:"#F07178"}},"g-count-down"),t("span",{style:{color:"#89DDFF"}}," "),t("span",{style:{color:"#C792EA"}},"type"),t("span",{style:{color:"#89DDFF"}},"="),t("span",{style:{color:"#89DDFF"}},'"'),t("span",{style:{color:"#C3E88D"}},"button"),t("span",{style:{color:"#89DDFF"}},'"'),t("span",{style:{color:"#89DDFF"}}," />")]),o(`
+`),t("span",{class:"line"},[t("span",{style:{color:"#A6ACCD"}},"  "),t("span",{style:{color:"#89DDFF"}},"</"),t("span",{style:{color:"#F07178"}},"div"),t("span",{style:{color:"#89DDFF"}},">")]),o(`
+`),t("span",{class:"line"},[t("span",{style:{color:"#A6ACCD"}},"  "),t("span",{style:{color:"#89DDFF"}},"<"),t("span",{style:{color:"#F07178"}},"p"),t("span",{style:{color:"#89DDFF"}},">"),t("span",{style:{color:"#A6ACCD"}},"{{ msg1 }}"),t("span",{style:{color:"#89DDFF"}},"</"),t("span",{style:{color:"#F07178"}},"p"),t("span",{style:{color:"#89DDFF"}},">")]),o(`
+`),t("span",{class:"line"},[t("span",{style:{color:"#A6ACCD"}},"  "),t("span",{style:{color:"#89DDFF"}},"<"),t("span",{style:{color:"#F07178"}},"div"),t("span",{style:{color:"#89DDFF"}},">")]),o(`
+`),t("span",{class:"line"},[t("span",{style:{color:"#A6ACCD"}},"    "),t("span",{style:{color:"#89DDFF"}},"<"),t("span",{style:{color:"#F07178"}},"g-count-down"),t("span",{style:{color:"#89DDFF"}}," "),t("span",{style:{color:"#C792EA"}},"type"),t("span",{style:{color:"#89DDFF"}},"="),t("span",{style:{color:"#89DDFF"}},'"'),t("span",{style:{color:"#C3E88D"}},"input"),t("span",{style:{color:"#89DDFF"}},'"'),t("span",{style:{color:"#89DDFF"}}," />")]),o(`
+`),t("span",{class:"line"},[t("span",{style:{color:"#A6ACCD"}},"  "),t("span",{style:{color:"#89DDFF"}},"</"),t("span",{style:{color:"#F07178"}},"div"),t("span",{style:{color:"#89DDFF"}},">")]),o(`
+`),t("span",{class:"line"},[t("span",{style:{color:"#89DDFF"}},"</"),t("span",{style:{color:"#F07178"}},"template"),t("span",{style:{color:"#89DDFF"}},">")]),o(`
+`),t("span",{class:"line"}),o(`
+`),t("span",{class:"line"},[t("span",{style:{color:"#89DDFF"}},"<"),t("span",{style:{color:"#F07178"}},"script"),t("span",{style:{color:"#89DDFF"}},">")]),o(`
+`),t("span",{class:"line"},[t("span",{style:{color:"#89DDFF","font-style":"italic"}},"import"),t("span",{style:{color:"#A6ACCD"}}," "),t("span",{style:{color:"#89DDFF"}},"{"),t("span",{style:{color:"#F07178"}}," "),t("span",{style:{color:"#A6ACCD"}},"defineComponent"),t("span",{style:{color:"#F07178"}}," "),t("span",{style:{color:"#89DDFF"}},"}"),t("span",{style:{color:"#A6ACCD"}}," "),t("span",{style:{color:"#89DDFF","font-style":"italic"}},"from"),t("span",{style:{color:"#A6ACCD"}}," "),t("span",{style:{color:"#89DDFF"}},"'"),t("span",{style:{color:"#C3E88D"}},"vue"),t("span",{style:{color:"#89DDFF"}},"'"),t("span",{style:{color:"#89DDFF"}},";")]),o(`
+`),t("span",{class:"line"}),o(`
+`),t("span",{class:"line"},[t("span",{style:{color:"#89DDFF","font-style":"italic"}},"export"),t("span",{style:{color:"#A6ACCD"}}," "),t("span",{style:{color:"#89DDFF","font-style":"italic"}},"default"),t("span",{style:{color:"#A6ACCD"}}," "),t("span",{style:{color:"#82AAFF"}},"defineComponent"),t("span",{style:{color:"#A6ACCD"}},"("),t("span",{style:{color:"#89DDFF"}},"{")]),o(`
+`),t("span",{class:"line"},[t("span",{style:{color:"#A6ACCD"}},"  "),t("span",{style:{color:"#F07178"}},"setup"),t("span",{style:{color:"#89DDFF"}},"()"),t("span",{style:{color:"#A6ACCD"}}," "),t("span",{style:{color:"#89DDFF"}},"{")]),o(`
+`),t("span",{class:"line"},[t("span",{style:{color:"#F07178"}},"    "),t("span",{style:{color:"#89DDFF","font-style":"italic"}},"return"),t("span",{style:{color:"#F07178"}}," "),t("span",{style:{color:"#89DDFF"}},"{")]),o(`
+`),t("span",{class:"line"},[t("span",{style:{color:"#F07178"}},"      msg"),t("span",{style:{color:"#89DDFF"}},":"),t("span",{style:{color:"#F07178"}}," "),t("span",{style:{color:"#89DDFF"}},"'"),t("span",{style:{color:"#C3E88D"}}," 倒计时按钮组件"),t("span",{style:{color:"#89DDFF"}},"'"),t("span",{style:{color:"#89DDFF"}},",")]),o(`
+`),t("span",{class:"line"},[t("span",{style:{color:"#F07178"}},"      msg1"),t("span",{style:{color:"#89DDFF"}},":"),t("span",{style:{color:"#F07178"}}," "),t("span",{style:{color:"#89DDFF"}},"'"),t("span",{style:{color:"#C3E88D"}}," 倒计时按钮输入组件"),t("span",{style:{color:"#89DDFF"}},"'")]),o(`
+`),t("span",{class:"line"},[t("span",{style:{color:"#F07178"}},"    "),t("span",{style:{color:"#89DDFF"}},"};")]),o(`
+`),t("span",{class:"line"},[t("span",{style:{color:"#F07178"}},"  "),t("span",{style:{color:"#89DDFF"}},"}")]),o(`
+`),t("span",{class:"line"},[t("span",{style:{color:"#89DDFF"}},"}"),t("span",{style:{color:"#A6ACCD"}},")"),t("span",{style:{color:"#89DDFF"}},";")]),o(`
+`),t("span",{class:"line"},[t("span",{style:{color:"#89DDFF"}},"</"),t("span",{style:{color:"#F07178"}},"script"),t("span",{style:{color:"#89DDFF"}},">")]),o(`
+`),t("span",{class:"line"}),o(`
+`),t("span",{class:"line"},[t("span",{style:{color:"#89DDFF"}},"<"),t("span",{style:{color:"#F07178"}},"style"),t("span",{style:{color:"#89DDFF"}},"></"),t("span",{style:{color:"#F07178"}},"style"),t("span",{style:{color:"#89DDFF"}},">")])])])],-1),k=E('<h2 id="g-count-down-参数" tabindex="-1">g-count-down 参数 <a class="header-anchor" href="#g-count-down-参数" aria-label="Permalink to &quot;g-count-down 参数&quot;">​</a></h2><table><thead><tr><th>参数</th><th>类型</th><th>默认</th><th>可选值</th><th>说明</th></tr></thead><tbody><tr><td>type</td><td>any</td><td>button</td><td>button/input</td><td>类型</td></tr><tr><td>value</td><td>any</td><td>-</td><td>-</td><td>绑定值</td></tr><tr><td>count</td><td>number</td><td>60</td><td>-</td><td>倒计时时间</td></tr><tr><td>beforeStartFunc</td><td>()=&gt;promise</td><td>-</td><td>-</td><td>倒计时之前执行的函数，返回 true 才会开始执行</td></tr><tr><td></td><td></td><td></td><td></td><td></td></tr></tbody></table><h2 id="g-count-input-参数" tabindex="-1">g-count-input 参数 <a class="header-anchor" href="#g-count-input-参数" aria-label="Permalink to &quot;g-count-input 参数&quot;">​</a></h2><table><thead><tr><th>参数</th><th>类型</th><th>默认</th><th>可选值</th><th>说明</th></tr></thead><tbody><tr><td>type</td><td>any</td><td>-</td><td>input</td><td>类型</td></tr><tr><td>value</td><td>any</td><td>-</td><td>-</td><td>绑定值</td></tr><tr><td>count</td><td>number</td><td>60</td><td>-</td><td>倒计时时间</td></tr><tr><td>sendCodeApi</td><td>()=&gt;promise</td><td>-</td><td>-</td><td>倒计时之前执行的函数，返回 true 才会开始执行</td></tr></tbody></table>',4);function N(n,s,u,l,r,p){const e=D("render-demo-0"),a=D("demo");return _(),h("div",null,[f,b,v,w,y(a,{customClass:"undefined",sourceCode:`<template>
+  <p>{{ msg }}</p>
+  <div>
+    <g-count-down type="button" />
+  </div>
+  <p>{{ msg1 }}</p>
+  <div>
+    <g-count-down type="input" />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  setup() {
+    return {
+      msg: ' 倒计时按钮组件',
+      msg1: ' 倒计时按钮输入组件'
+    };
+  }
+});
+<\/script>
+
+<style></style>
+`},{highlight:i(()=>[x]),default:i(()=>[y(e)]),_:1}),k])}const q=m(g,[["render",N]]);export{S as __pageData,q as default};
